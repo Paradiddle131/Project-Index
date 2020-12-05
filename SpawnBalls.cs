2456d8a3b86670f03e3ae68c0e93906806082c30
SAM <ebhan96@hotmail.com>
2020-12-05 23:48:50 +0300

    TurnTomToGreen

--- SpawnBalls.cs
+++ SpawnBalls.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class SpawnBalls : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class SpawnBalls : MonoBehaviour
     [SerializeField] public int numberOfToms;
     [SerializeField] public float radius;
 
+    Dictionary<System.DateTime, GameObject> toms = new Dictionary<System.DateTime, GameObject>();
+
     Vector2 GetRandomPoint()
     {
         float randomX = Random.Range(-boundaryX, boundaryX);
@@ -20,58 +23,44 @@ public class SpawnBalls : MonoBehaviour
     void SpawnObject(GameObject go, int amount)
     {
         if (go == null) return;
-        //float objectRadius = go.GetComponent<Collider2D>().bounds.extents.x;
-        //GameObject[] toms = GameObject.FindGameObjectsWithTag("Tom").;
-        //Debug.Log('x');
         int countSpawned = 0;
         int numIterated = 0;
-        //radius = Tom.transform.lossyScale[0] / 2;
-        //ContactFilter2D contactFilter = new ContactFilter2D();
-        //Debug.Log(new Vector2(-2.124238f, 2.313077f));
-        //Debug.Log(new Vector2(-2.114317f, 2.082304f));
-        LayerMask mask = LayerMask.GetMask("Boundary");
         do
         {
+            System.Threading.Thread.Sleep(100);
             Vector2 randomPoint = GetRandomPoint();
-            //Collider2D circleCollider = Physics2D.OverlapPoint(randomPoint);
-            if (Physics2D.OverlapCircle(randomPoint, radius, mask))
+            if (Physics2D.OverlapCircle(randomPoint, radius))
             {
                 Debug.Log("COLLIDED ON: " + randomPoint);
                 numIterated++;
-                if (numIterated == amount*2)
+                if (numIterated == amount * 2)
                 {
                     break;
                 }
                 continue;
             }
-            GameObject tmp;
-            tmp = Instantiate(go);
-            tmp.transform.position = randomPoint;
-
-            //GameObject tmp = Instantiate(go);
-            //tmp.gameObject.transform.position = randomPoint;
+            GameObject tom;
+            tom = Instantiate(go);
+            tom.transform.position = randomPoint;
+            toms.Add(System.DateTime.Now, tom);
             countSpawned++;
+            //TurnTomToGreen(tom);
+            TurnTomToGreen(go);
         } while (countSpawned != amount);
-
-
-        //for (int i = 0; i < amount; i++)
-        //{
-        //    Vector2 randomPoint = GetRandomPoint();
-        //    Collider2D CollisionWithEnemy = Physics2D.OverlapCircle(randomPoint, objectRadius);
-        //    if (CollisionWithEnemy == false)
-        //    {
-        //        GameObject tmp = Instantiate(go);
-        //        tmp.gameObject.transform.position = randomPoint;
-        //    }
-        //}
     }
     void Start()
     {
-        SpawnObject(Tom, numberOfToms);
+        //SpawnObject(Tom, numberOfToms);
+    }
+
+    void TurnTomToGreen(GameObject go)
+    {
+        SpriteRenderer renderer = go.GetComponent<SpriteRenderer>();
+        renderer.color = new Color(0f, 1f, 0f, 1f);
     }
 
     void Update()
     {
-
+        SpawnObject(Tom, 1);
     }
 }
